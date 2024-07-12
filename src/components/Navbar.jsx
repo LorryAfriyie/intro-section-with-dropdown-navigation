056d7e69969891b142af65c432b3a021517a2add
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-07-12 04:12:18 +0200

    Flex property changes and navbar code changes
    Changing navbar code structure and changing flex property to adjust link item positioning.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,58 +1,63 @@
 export const Navbar = () => {
   return (
-    <nav className="navbar" id="navbar">
-      <div className="navbar__brand-title">snap</div>
+    <header>
+      <nav className="navbar" id="navbar">
+        <div className="navbar__brand-title">snap</div>
 
-      <button className="navbar__open">
-        <img src="/images/icon-menu.svg" alt="" width={"40"} height={"24"} />
-      </button>
+        {/* <button className="navbar__open">
+          <img src="/images/icon-menu.svg" alt="" width={"40"} height={"24"} />
+        </button> */}
 
-      <div className="navbar__menu">
-        <button className="navbar__close">
-          <img
-            src="/images/icon-close-menu.svg"
-            alt=""
-            width={"28"}
-            height={"27"}
-          />
-        </button>
+        <div className="navbar__menu">
+          {/* <button className="navbar__close">
+            <img
+              src="/images/icon-close-menu.svg"
+              alt=""
+              width={"28"}
+              height={"27"}
+            />
+          </button> */}
 
-        <ul className="navbar__links">
-          <li className="navbar__item">
-            <a href="" className="navbar__link">
-              Features
-            </a>
-          </li>
+          <ul className="navbar__links">
+            <li className="navbar__item">
+              <a href="" className="navbar__link">
+                Features
+              </a>
+            </li>
 
-          <li className="navbar__item">
-            <a href="" className="navbar__link">
-              Companies
-            </a>
-          </li>
+            <li className="navbar__item">
+              <a href="" className="navbar__link">
+                Companies
+              </a>
+            </li>
 
-          <li className="navbar__item">
-            <a href="" className="navbar__link">
-              Careers
-            </a>
-          </li>
+            <li className="navbar__item">
+              <a href="" className="navbar__link">
+                Careers
+              </a>
+            </li>
 
-          <li className="navbar__item">
-            <a href="" className="navbar__link">
-              About
-            </a>
-          </li>
-        </ul>
+            <li className="navbar__item">
+              <a href="" className="navbar__link">
+                About
+              </a>
+            </li>
 
-        <div className="navbar__auth">
-            <a href="" className="login">
-              Login
-            </a>
+            <li className="navbar__item">
+              <a href="">
+                <button className="login">Login</button>
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="">
+                <button className="register">Register</button>
+              </a>
+            </li>
+          </ul>
+        </div>
 
-            <a href="" className="register">
-              Register
-            </a>
-          </div>
-      </div>
-    </nav>
+        <div className="navbar__auth"></div>
+      </nav>
+    </header>
   );
 };
