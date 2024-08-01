cc985a3cde70dac539cdd542915b09b21665cdd9
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-08-01 05:12:40 +0200

    Hero Component
    Creating hero component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
 
 import "./App.css";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+export const Hero = () => {
+  return (
+    <main className="hero">
+      <div className="hero__content-grid">
+        <div className="hero__info">Info here</div>
+        <div className="hero__img-container"></div>
+      </div>
+    </main>
+  );
+};
