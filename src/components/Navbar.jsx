92637197766c93e14f2b35d65e8fe74b5cd9d5a8
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-08-17 03:23:06 +0200

    Hamburger and close button adjustment
    Changing hamburger and close svg from being two separate elements to be able to interchange appearance between both when menu is active vs when it is not active.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,46 +4,31 @@ import { NavbarLinks, NavbarAuth } from "./NavbarLinks";
 export const Navbar = () => {
   const menu = useRef(null),
     body = document.querySelector("body"),
-    btnOpen = useRef(null),
-    btnClose = useRef(null);
+    btnOpen = useRef(null);
 
   useEffect(() => {
     function openMenu() {
-      btnClose.current.focus();
-      body.classList.add("overlay");
-
       const visibility = menu.current.getAttribute("data-visible");
 
       if (visibility === "false") {
         menu.current.setAttribute("data-visible", true);
         btnOpen.current.setAttribute("aria-expanded", true);
+        body.classList.add("overlay");
       }
-    }
-
-    function closeMenu() {
-      btnClose.current.focus();
-      body.classList.remove("overlay");
-
-      const visibility = menu.current.getAttribute("data-visible");
 
       if (visibility === "true") {
         menu.current.setAttribute("data-visible", false);
         btnOpen.current.setAttribute("aria-expanded", false);
+        body.classList.remove("overlay");
       }
     }
 
     btnOpen.current.addEventListener("click", openMenu);
-
-    btnClose.current.addEventListener("click", closeMenu);
   });
 
   return (
     <nav className="navbar" id="navbar">
       <div className="navbar__brand-title">snap</div>
-
-      <span id="nav-label" hidden>
-        Navigation
-      </span>
 
       <button
         type="button"
@@ -51,13 +36,7 @@ export const Navbar = () => {
         className="navbar__open"
         aria-expanded="false"
       >
-        <span>Menu</span>
-        {/*  <img
-          src="/images/icon-menu.svg"
-          alt="open-button"
-          width={"40"}
-          height={"24"}
-        /> */}
+        <span className="sr-only">Menu</span>
       </button>
 
       <div
@@ -66,15 +45,6 @@ export const Navbar = () => {
         data-visible="false"
         ref={menu}
       >
-        <button
-          type="button"
-          className="navbar__close"
-          ref={btnClose}
-          aria-label="close"
-        >
-          <img src="/images/icon-close-menu.svg" alt="close-button" />
-        </button>
-
         <NavbarLinks />
 
         <NavbarAuth />
